3820f36a7127c3900729119d07316779c5e43c84
Rupayan Midya <rupofficial2002@gmail.com>
2026-02-26 02:59:50 +0530

    Fix model folder path

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const Product = require("../model/product");
+const Product = require("../model/products");
 
 const getAllProducts = async (req, res) => {
   const products = await Product.find({});
